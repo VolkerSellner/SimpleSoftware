fe625b22d9ccde27cf5e5469d21965bbe2545914
Sellner <sel230598@spengergasse.at>
2024-08-31 13:13:02 +0200

    Datenbankanbindung und CRUD

---
+++ daten/daten_code.js
@@ -0,0 +1,46 @@
+// daten/daten_code.js
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function getDb() {
+    await client.connect();
+    return client.db('SimpleSoftware').collection('names');
+}
+
+async function saveName(req, res) {
+    try {
+        const db = await getDb();
+        const result = await db.insertOne({ firstName: req.body.firstName, lastName: req.body.lastName });
+        res.status(201).send({ message: 'Name gespeichert', id: result.insertedId });
+    } catch (err) {
+        res.status(500).send({ error: 'Fehler beim Speichern des Namens' });
+    }
+}
+
+async function deleteName(req, res) {
+    try {
+        const db = await getDb();
+        const result = await db.deleteOne({ _id: new require('mongodb').ObjectId(req.params.id) });
+        if (result.deletedCount === 1) {
+            res.status(200).send({ message: 'Name gelöscht' });
+        } else {
+            res.status(404).send({ message: 'Name nicht gefunden' });
+        }
+    } catch (err) {
+        res.status(500).send({ error: 'Fehler beim Löschen des Namens' });
+    }
+}
+
+async function getNames(req, res) {
+    try {
+        const db = await getDb();
+        const names = await db.find().toArray();
+        res.status(200).json(names);
+    } catch (err) {
+        res.status(500).send({ error: 'Fehler beim Abrufen der Namen' });
+    }
+}
+
+module.exports = { saveName, deleteName, getNames };
